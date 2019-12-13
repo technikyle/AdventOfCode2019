111494d31c503ac23350f5d6e244e63c82773f51
Kyle Rokita <kyle@Kyles-MacBook-Pro.local>
2019-12-12 23:22:46 -0800

    Day7

---
+++ Day7/Day7Tests/Day7Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Day7Tests.swift
+//  Day7Tests
+//
+//  Created by Kyle Rokita on 12/12/19.
+//  Copyright © 2019 Kyle Rokita. All rights reserved.
+//
+
+import XCTest
+@testable import Day7
+
+class Day7Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
